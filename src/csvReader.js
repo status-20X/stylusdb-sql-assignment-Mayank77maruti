763dd818711060120a1d66ec30e9165a2d0f35fa
Mayank77maruti <125661248+Mayank77maruti@users.noreply.github.com>
2024-05-10 15:31:34 +0530

    completed till 20

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,6 +1,22 @@
 const fs = require("fs");
 const csv = require("csv-parser");
-const { parse } = require("json2csv");
+
+// Helper function to convert data
+function convertData(data) {
+  let writeContent = "";
+  let rowsInserted = 0;
+
+  data.forEach((row) => {
+    for (const key in row) {
+      writeContent += `${row[key]},`;
+    }
+    writeContent = writeContent.slice(0, -1);
+    writeContent += "\n";
+    rowsInserted += 1;
+  });
+
+  return { writeContent, rowsInserted };
+}
 
 function readCSV(filePath) {
   const results = [];
@@ -8,7 +24,9 @@ function readCSV(filePath) {
   return new Promise((resolve, reject) => {
     fs.createReadStream(filePath)
       .pipe(csv())
-      .on("data", (data) => results.push(data))
+      .on("data", (data) => {
+        results.push(data);
+      })
       .on("end", () => {
         resolve(results);
       })
@@ -18,9 +36,36 @@ function readCSV(filePath) {
   });
 }
 
-async function writeCSV(filename, data) {
-  const csv = parse(data);
-  fs.writeFileSync(filename, csv);
+async function writeCSV(filePath, data) {
+  const { writeContent, rowsInserted } = convertData(data);
+
+  let fields = "";
+
+  for (const key in data[0]) {
+    fields += `${key},`;
+  }
+  fields = fields.slice(0, -1);
+  fields += "\n" + writeContent;
+
+  fs.writeFile(filePath, fields, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+    }
+  });
 }
 
-module.exports = {readCSV, writeCSV};
+async function updateCSV(filePath, data) {
+  const { writeContent, rowsInserted } = convertData(data);
+
+  fs.appendFile(filePath, writeContent, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+    }
+  });
+
+  return { rowsInserted };
+}
+
+module.exports = { readCSV, writeCSV, updateCSV };
--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -1,5 +1,6 @@
 const { readCSV } = require("../../src/csvReader");
-const { parseQuery } = require("../../src/queryParser");
+const { parseSelectQuery, parseJoinClause } = require("../../src/queryParser");
+const { executeSELECTQuery } = require("../../src/queryExecutor");
 
 test("Read CSV File", async () => {
   const data = await readCSV("./student.csv");
@@ -11,7 +12,7 @@ test("Read CSV File", async () => {
 
 test("Parse SQL Query", () => {
   const query = "SELECT id, name FROM student";
-  const parsed = parseQuery(query);
+  const parsed = parseSelectQuery(query);
   expect(parsed).toEqual({
     fields: ["id", "name"],
     table: "student",
@@ -21,8 +22,8 @@ test("Parse SQL Query", () => {
     joinType: null,
     groupByFields: null,
     hasAggregateWithoutGroupBy: false,
-    orderByFields: null,
-    limit: null,
     isDistinct: false,
+    limit: null,
+    orderByFields: null,
   });
 });
